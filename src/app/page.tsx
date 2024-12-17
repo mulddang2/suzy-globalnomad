7bcca8499906596987e3b2d552910254398c8825
Suzy-Lee <>
2024-12-17 12:55:39 +0900

    chore: svg 컴포넌트로 import 가능하도록 셋팅 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import StarFill from '../../public/icons/star-fill.svg';
 export default function Home() {
-  return <h1>글로벌 노마드</h1>;
+  return (
+    <>
+      <h1>글로벌 노마드</h1>
+      <StarFill />
+    </>
+  );
 }

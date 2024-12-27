bc775090780f8ac26b25c74d62b9d54f35ac0b26
Suzy-Lee <>
2024-12-27 12:11:23 +0900

    :recycle: refactor: page 내에 기재한 cardSectionList를 CardList 컴포넌트로 분리

---
+++ src/components/profile-experience/CardList.css.ts
@@ -0,0 +1,74 @@
+import { fontSizes, fontWeights } from '@/styles/fontStyles.css';
+import { style } from '@vanilla-extract/css';
+import { global } from '@/styles/global.css';
+
+export const cardSectionList = style({
+  display: 'grid',
+  marginTop: '3rem',
+  rowGap: '1.6rem',
+  maxWidth: '80rem',
+  margin: '0 auto', // TODO: 페이지 레이아웃 수정 시 제거하기
+});
+
+export const cardSection = style({
+  display: 'grid',
+  gridTemplateColumns: '20.4rem 1fr',
+  columnGap: '2.4rem',
+  backgroundColor: '#fff',
+  boxShadow: '0px 4px 16px rgba(17, 34, 17, 0.05)',
+  borderRadius: '2.4rem',
+  overflow: 'hidden',
+});
+
+export const cardImageContainer = style({
+  width: '204px',
+  height: '204px',
+  overflow: 'hidden',
+  objectFit: 'contain',
+  position: 'relative',
+});
+
+export const responsiveImage = style({
+  objectFit: 'cover',
+  width: '100%',
+  height: '100%',
+});
+
+export const cardContentLayout = style({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  padding: '1.2rem 0 1.3rem',
+});
+
+export const cardTopLayout = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.6rem',
+});
+
+export const starRatingLayout = style([
+  fontSizes.textLg,
+  fontWeights.regular,
+  {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '0.6rem',
+  },
+]);
+
+export const h3Title = style([
+  fontSizes.text2lg,
+  fontWeights.bold,
+  {
+    color: global.color.nomadBlack,
+  },
+]);
+
+export const cardBottomLayout = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+});
+
+export const priceText = style([fontSizes.textXl, fontWeights.medium]);
---
+++ src/components/profile-experience/CardList.tsx
@@ -0,0 +1,43 @@
+import MeatballIcon from '@/assets/icons/meatball.svg';
+import StarIcon from '@/assets/icons/star-fill.svg';
+import testImage from '@/assets/images/test-image-experience1.png';
+import Image from 'next/image';
+import React from 'react';
+import * as styles from './CardList.css';
+
+export default function CardList() {
+  return (
+    <section className={styles.cardSectionList}>
+      <div className={styles.cardSection}>
+        <div className={styles.cardImageContainer}>
+          <Image
+            src={testImage}
+            width={520}
+            height={272}
+            loading='lazy'
+            alt={'체험 이미지'}
+            className={styles.responsiveImage}
+          />
+        </div>
+        <div className={styles.cardContentLayout}>
+          <div className={styles.cardTopLayout}>
+            <div className={styles.starRatingLayout}>
+              {/* TODO: rating 공용 컴포넌트 적용하기 */}
+              <StarIcon width={19} height={19} />
+              <span> 4.9</span>
+              <span> (293)</span>
+            </div>
+            <h3 className={styles.h3Title}>함께 배우면 즐거운 스트릿 댄스</h3>
+          </div>
+          <div className={styles.cardBottomLayout}>
+            <p className={styles.priceText}>₩10,000</p>
+            {/* TODO: dropdown 공용 컴포넌트 적용하기 */}
+            <button>
+              <MeatballIcon width={40} height={40} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}

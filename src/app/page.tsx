f4b095941731b05dde09b48c524819cecebb4708
나승엽 <a01031928263@gmail.com>
2024-12-28 17:47:25 +0900

    Co-authored-by: GANGYIKIM <GANGYIKIM@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,3 @@
-'use client';
-
-import DropDownA from '@/components/dropdown/DropDownA';
-import DropDownB from '@/components/dropdown/DropDownB';
-import React from 'react';
-
 export default function Home() {
-  const handleSelect = (item: string) => {
-    alert(`Selected item: ${item}`);
-  };
-
-  return (
-    <main>
-      <DropDownA options={['수정하기', '삭제하기', 'sadf', 'asdfqr']} onSelect={handleSelect} />
-      <DropDownB options={['가격이 낮은 순', '가격이 높은 순']} onSelect={handleSelect} />
-    </main>
-  );
+  return <></>;
 }

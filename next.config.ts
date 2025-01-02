575a1748371c5ae16a4ed7eea3a25f393b3484d4
Suzy-Lee <>
2025-01-02 20:22:55 +0900

    :bug: fix: 컴포넌트명 대문자로 변경, config 파일에서 require 대신 import 사용

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 import type { NextConfig } from 'next';
 
-const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig: NextConfig = {
---
+++ src/app/profile/my-activities/page.tsx
@@ -0,0 +1,15 @@
+import CardList from '@/components/profile-activities/CardList';
+import * as styles from './page.css';
+
+export default function ActivitiesPage() {
+
+  return (
+    <div className={styles.activitiesPageContainer}>
+      <div className={styles.topLayout}>
+        <h2 className={styles.h2Title}>내 체험 관리</h2>
+        <button className={styles.createButton}>체험 등록하기</button>
+      </div>
+      <CardList />
+    </div>
+  );
+}

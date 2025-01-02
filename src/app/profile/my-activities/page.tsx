97c1486ab3f7482f99c22cde58f98c85993d9ec8
Suzy-Lee <>
2025-01-02 20:29:08 +0900

    :bug: fix: 컴포넌트명 및 스타일변수명 수정

--- src/app/profile/my-activities/page.tsx
+++ src/app/profile/my-activities/page.tsx
@@ -1,10 +1,9 @@
 import CardList from '@/components/profile-activities/CardList';
 import * as styles from './page.css';
 
-export default function ActivitiesPage() {
-
+export default function MyActivitiesPage() {
   return (
-    <div className={styles.activitiesPageContainer}>
+    <div className={styles.myActivitiesPageContainer}>
       <div className={styles.topLayout}>
         <h2 className={styles.h2Title}>내 체험 관리</h2>
         <button className={styles.createButton}>체험 등록하기</button>

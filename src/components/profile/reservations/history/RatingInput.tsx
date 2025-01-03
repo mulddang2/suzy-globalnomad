29ddfcaa652c481fcb0659c48fff8e0decdb08b4
박지민 <25wlals@gmail.com>
2025-01-03 22:54:23 +0900

    ✨ Feat: 별점 입력 컴포넌트 구현

--- src/components/profile/reservations/history/RatingInput.tsx
+++ src/components/profile/reservations/history/RatingInput.tsx
@@ -6,17 +6,29 @@ import * as styles from './RatingInput.css';
 export default function RatingInput() {
   const [stars, setStars] = useState([false, false, false, false, false]);
 
-  const handleClick = () => {
-    setStars(stars);
+  const handleClick = (i: number) => {
+    const newStars = [];
+
+    let j = 0;
+    while (j < 5) {
+      if (j <= i) {
+        newStars[j] = true;
+      } else {
+        newStars[j] = false;
+      }
+      j++;
+    }
+
+    setStars(newStars);
   };
 
   return (
     <div className={styles.content}>
       {stars.map((data, i) =>
         data ? (
-          <StarFill className={styles.star} key={i} onClick={handleClick} />
+          <StarFill className={styles.star} key={i} onClick={() => handleClick(i)} />
         ) : (
-          <StarEmpty className={styles.star} key={i} onClick={handleClick} />
+          <StarEmpty className={styles.star} key={i} onClick={() => handleClick(i)} />
         ),
       )}
     </div>

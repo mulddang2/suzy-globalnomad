372571d4bbabb9e46acc4a2377dd343f7fdb1344
Your Name <you@example.com>
2025-01-04 23:48:51 +0900

    🐛 Fix:build 에러 해결
    fix:build 에러 해결

--- next.config.ts
+++ next.config.ts
@@ -23,17 +23,6 @@ const nextConfig: NextConfig = {
 
     return config;
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
-        port: '',
-        pathname: '/**',
-        search: '',
-      },
-    ],
-  },
 
   transpilePackages: ['@company/design-system'],
 };

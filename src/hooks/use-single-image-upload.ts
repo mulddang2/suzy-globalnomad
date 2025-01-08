542d5fccbfb8389f84e7dc30a4321ddc7f7d394e
Suzy-Lee <>
2025-01-09 00:29:56 +0900

    :sparkles: feat: 단일이미지, 다중이미지 업로드 관련 커스텀 훅 추가

---
+++ src/hooks/use-single-image-upload.ts
@@ -0,0 +1,29 @@
+import { ChangeEvent, useState } from 'react';
+
+const useSingleImageUpload = () => {
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
+  const [imageFile, setImageFile] = useState<File | null>(null);
+
+  const encodeFileToBase64 = (file: File) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      if (typeof reader.result === 'string') {
+        setImageSrc(reader.result);
+      }
+    };
+  };
+
+  const handleSingleImagePreview = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const selectedFile = e.target.files[0];
+      setImageFile(selectedFile);
+      encodeFileToBase64(selectedFile);
+    }
+  };
+
+  return { imageSrc, imageFile, handleSingleImagePreview };
+};
+
+export default useSingleImageUpload;

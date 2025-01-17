623e8308e0647665c54df421e1153ccdca98c9a6
박지민 <25wlals@gmail.com>
2025-01-17 14:45:16 +0900

    ✨ Feat: 예약 승인, 거절 api 구현

--- src/components/profile/reservations/status/ReservationItem.tsx
+++ src/components/profile/reservations/status/ReservationItem.tsx
@@ -1,9 +1,19 @@
+import { confirmReservation, declineReservation } from '@/apis/my-activity-board';
 import { Button } from '@/components/button/Button';
 import { Reservation } from './MyActivityModal';
 import * as styles from './ReservationItem.css';
 
 export default function ReservationItem(props: { item: Reservation; key: number }) {
   // console.log('resItem: ', props.item);
+
+  const handleConfirm = () => {
+    confirmReservation(props.item.activityId, props.item.id);
+    //승인 후 바로 화면 업데이트 안됨
+  };
+
+  const handleDecline = () => {
+    declineReservation(props.item.activityId, props.item.id);
+  };
 
   return (
     <div className={styles.item}>
@@ -17,8 +27,8 @@ export default function ReservationItem(props: { item: Reservation; key: number 
       </div>
       {props.item.status === 'pending' && (
         <div className={styles.buttonBar}>
-          <Button label='승인하기' className={styles.button} variant='solid' />
-          <Button label='거절하기' className={styles.button} variant='outline' />
+          <Button label='승인하기' className={styles.button} variant='solid' onClick={handleConfirm} />
+          <Button label='거절하기' className={styles.button} variant='outline' onClick={handleDecline} />
         </div>
       )}
       {props.item.status === 'confirmed' && (

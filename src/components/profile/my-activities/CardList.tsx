3ba27a8c896f924c9f4eb537d32ed204cfb6c2f7
Suzy-Lee <>
2025-01-18 09:37:27 +0900

    :sparkles: feat: 관리 페이지 - 스켈레톤 UI 추가

--- src/components/profile/my-activities/CardList.tsx
+++ src/components/profile/my-activities/CardList.tsx
@@ -6,7 +6,7 @@ import { useDeleteActivity } from '@/hooks/use-delete-activity';
 import { useMyActivities } from '@/hooks/use-my-activities';
 import { MyActivitiesList } from '@/types/my-activities-list';
 import { formatToKor } from '@/utils/format-to-kor';
-import { CircularProgress } from '@mui/material';
+import { CircularProgress, Skeleton } from '@mui/material';
 import { useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import Image from 'next/image';
@@ -15,7 +15,7 @@ import { useEffect, useRef, useState } from 'react';
 import * as styles from './CardList.css';
 
 export default function CardList() {
-  const { data, fetchNextPage, isFetchingNextPage } = useMyActivities();
+  const { data, isLoading, fetchNextPage, isFetchingNextPage } = useMyActivities();
   const targetRef = useRef<HTMLDivElement>(null);
   const router = useRouter();
   const [, setIsDropdownOpen] = useState(false);
@@ -62,56 +62,83 @@ export default function CardList() {
   }, [fetchNextPage]);
   return (
     <section>
-      {data?.pages.map((page) => (
-        <div key={page.cursorId} className={styles.cardSectionList}>
-          {page.activities.map((activity: MyActivitiesList) => (
-            <div className={styles.cardSection} key={activity.id}>
+      {isLoading && (
+        <div className={styles.cardSectionList}>
+          {new Array(3).fill('').map((_, index) => (
+            <div className={styles.cardSection} key={`skeleton-${index}`}>
               <div className={styles.cardImageContainer}>
-                <Image
-                  src={activity.bannerImageUrl}
-                  fill
-                  sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-                  loading='lazy'
-                  alt={'체험 이미지'}
-                  className={styles.responsiveImage}
-                />
+                <Skeleton variant='rectangular' width='100%' height={200} animation='wave' />
               </div>
               <div className={styles.cardContentLayout}>
                 <div className={styles.cardTopLayout}>
-                  <div className={styles.starRatingLayout}>
-                    <StarIcon width={19} height={19} />
-                    <span> {activity.rating.toFixed(1)}</span>
-                    <span>({activity.reviewCount})</span>
-                  </div>
-                  <h3 className={styles.h3Title}>{activity.title}</h3>
+                  <Skeleton variant='text' width='60%' height={30} animation='wave' />
+                  <Skeleton variant='text' width='40%' height={20} animation='wave' />
                 </div>
                 <div className={styles.cardBottomLayout}>
-                  <p className={styles.priceText}>{formatToKor(activity.price)}</p>
-                  <div onClick={handleClick}>
-                    <DropDownA
-                      onSelect={(option) => {
-                        if (option === '수정하기') {
-                          router.push(`/profile/my-activities/edit/${activity.id}`);
-                        } else if (option === '삭제하기') {
-                          if (confirm('삭제 하시겠습니까?')) {
-                            handleDelete(activity.id);
-                          }
-                        }
-                      }}
-                      options={['수정하기', '삭제하기']}
-                    />
-                  </div>
+                  <Skeleton variant='text' width='50%' height={20} animation='wave' />
                 </div>
               </div>
             </div>
           ))}
         </div>
-      ))}
+      )}
+
+      {/* 데이터 렌더링 */}
+      {!isLoading &&
+        data?.pages.map((page) => (
+          <div key={page.cursorId} className={styles.cardSectionList}>
+            {page.activities.map((activity: MyActivitiesList) => (
+              <div className={styles.cardSection} key={activity.id}>
+                <div className={styles.cardImageContainer}>
+                  <Image
+                    src={activity.bannerImageUrl}
+                    fill
+                    sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+                    loading='lazy'
+                    alt={'체험 이미지'}
+                    className={styles.responsiveImage}
+                  />
+                </div>
+                <div className={styles.cardContentLayout}>
+                  <div className={styles.cardTopLayout}>
+                    <div className={styles.starRatingLayout}>
+                      <StarIcon width={19} height={19} />
+                      <span> {activity.rating.toFixed(1)}</span>
+                      <span>({activity.reviewCount})</span>
+                    </div>
+                    <h3 className={styles.h3Title}>{activity.title}</h3>
+                  </div>
+                  <div className={styles.cardBottomLayout}>
+                    <p className={styles.priceText}>{formatToKor(activity.price)}</p>
+                    <div onClick={handleClick}>
+                      <DropDownA
+                        onSelect={(option) => {
+                          if (option === '수정하기') {
+                            router.push(`/profile/my-activities/edit/${activity.id}`);
+                          } else if (option === '삭제하기') {
+                            if (confirm('삭제 하시겠습니까?')) {
+                              handleDelete(activity.id);
+                            }
+                          }
+                        }}
+                        options={['수정하기', '삭제하기']}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        ))}
+
+      {/* 추가 데이터 로딩 중일 때 로딩바 */}
       {isFetchingNextPage && (
         <div className={styles.loadingBarWrapper}>
           <CircularProgress color='inherit' />
         </div>
       )}
+
+      {/* Intersection Observer의 target */}
       <div ref={targetRef} />
     </section>
   );

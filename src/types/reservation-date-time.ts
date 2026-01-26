43f942fbc25640512c931365431403577ee3c229
mulddang2 <mulddang2@users.noreply.github.com>
2026-01-26 13:47:38 +0900

    fix: 예약 날짜 타입 추가

--- src/types/reservation-date-time.ts
+++ src/types/reservation-date-time.ts
@@ -2,6 +2,7 @@ import dayjs from 'dayjs';
 
 export type ReservationDateTime = {
   id?: number;
+  frontEndId?: string;
   date: dayjs.Dayjs | null;
   startTime: dayjs.Dayjs | null;
   endTime: dayjs.Dayjs | null;

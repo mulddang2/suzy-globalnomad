e3da78848d8cb1716836c06ae9362ec27df2eb90
mulddang2 <mulddang2@users.noreply.github.com>
2026-01-26 12:50:07 +0900

    feat: 내 체험 수정 커스텀 훅 추가

---
+++ src/hooks/use-my-activities-edit.ts
@@ -0,0 +1,16 @@
+import { axiosInstance } from '@/apis/axios-instance';
+import { MyActivitiesEditData } from '@/types/my-activities-edit-data';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+export const useMyActivitiesEdit = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async ({ activityId, data }: { activityId: number; data: MyActivitiesEditData }) => {
+      return axiosInstance.patch(`/my-activities/${activityId}`, data);
+    },
+    onSuccess: (_, variables) => {
+      queryClient.invalidateQueries({ queryKey: ['my-activities', variables.activityId] });
+    },
+  });
+};
